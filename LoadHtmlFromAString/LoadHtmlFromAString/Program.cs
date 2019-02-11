237aa2955db388e55af0b888cb241651c01a61b7
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-11 12:12:33 -0500

    Print a body tag in HTML page to the console successfull

--- LoadHtmlFromAString/LoadHtmlFromAString/Program.cs
+++ LoadHtmlFromAString/LoadHtmlFromAString/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,21 @@ namespace LoadHtmlFromAString
     {
         static void Main(string[] args)
         {
+            var html = @"<!DOCTYPE html>
+            <html>
+            <body>
+	                <h1>This is <b>bold</b> heading</h1>
+	                <p>This is <u>underlined</u> paragraph</p>
+	                <h2>This is <i>italic</i> heading</h2>
+            </body>
+            </html> ";
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var htmlBody = htmlDoc.DocumentNode.SelectSingleNode("//body");
+
+            Console.WriteLine(htmlBody.OuterHtml);
         }
     }
 }
