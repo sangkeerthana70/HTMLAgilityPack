4fba9163465f29ca3e84aae40641bb81ec5d83e9
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-02-14 12:08:43 -0500

    Load a web page and its contents with the agility pack

---
+++ GettingStartedWithHtmlAgilitypack/GettingStartedWithHtmlAgilitypack/Program.cs
@@ -0,0 +1,26 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStartedWithHtmlAgilitypack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // load a page from Internet
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("https://www.wikipedia.org/");
+            Console.WriteLine(document.DocumentNode.OuterHtml);
+
+            // load from a file
+            //HtmlDocument document2 = new HtmlDocument();
+            //Console.WriteLine(document2.Load(@"c:\Users\anuradha\source\repos\HtmlAgilityPack\SampleHtml.txt"));
+            
+                
+        }
+    }
+}
