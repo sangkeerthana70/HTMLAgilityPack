570ee0f622dd536f99f85ed5b1fc905fe74d0316
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-22 14:05:27 -0400

    Practice with Html AGility Pack

--- GettingStartedWithHtmlAgilitypack/GettingStartedWithHtmlAgilitypack/Program.cs
+++ GettingStartedWithHtmlAgilitypack/GettingStartedWithHtmlAgilitypack/Program.cs
@@ -13,14 +13,18 @@ namespace GettingStartedWithHtmlAgilitypack
         {
             // load a page from Internet
             HtmlWeb web = new HtmlWeb();
-            HtmlDocument document = web.Load("https://www.wikipedia.org/");
+            HtmlDocument document = web.Load("https://www.wikipedia.org/wiki/Plant");
+
+            // print contents of the web page
             Console.WriteLine(document.DocumentNode.OuterHtml);
+            var node = document.DocumentNode.SelectSingleNode("//*[@id=\"bodyContent\"]");
+            Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
 
             // load from a file
             //HtmlDocument document2 = new HtmlDocument();
             //Console.WriteLine(document2.Load(@"c:\Users\anuradha\source\repos\HtmlAgilityPack\SampleHtml.txt"));
-            
-                
+
+
         }
     }
 }
