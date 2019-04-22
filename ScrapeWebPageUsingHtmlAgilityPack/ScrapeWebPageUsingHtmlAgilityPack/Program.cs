886f86d9b53ee9ba229253abf6e780eefd214973
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-04-22 14:04:41 -0400

    Practice scraping using Html Agility Pack

---
+++ ScrapeWebPageUsingHtmlAgilityPack/ScrapeWebPageUsingHtmlAgilityPack/Program.cs
@@ -0,0 +1,30 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeWebPageUsingHtmlAgilityPack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string html = @"https://money.cnn.com/data/us_markets/";
+
+            HtmlWeb web = new HtmlWeb();
+
+            HtmlDocument htmlDoc = web.Load(html);
+
+            //var node = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"kp - wp - tab - MARKET_SUMMARY\"]/div[1]/div/div[1]/div[1]/div");
+            //HtmlNodeCollection rowsNodesList = htmlDoc.DocumentNode.SelectNodes("//*[@id=\"wsod_whatsMoving\"]");
+            var node = htmlDoc.DocumentNode.SelectNodes("//*[@id=\"wsod_whatsMoving\"]").First();
+            Console.WriteLine("node: " + node.InnerText);
+            
+
+            
+            
+        }
+    }
+}
---
+++ ScrapeWebPageUsingHtmlAgilityPack/ScrapeWebPageUsingHtmlAgilityPack/Stocks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeWebPageUsingHtmlAgilityPack
+{
+    class Stocks
+    {
+        public float Price { get; set; }
+        public float Change { get; set; }
+        public float PercentChange { get; set; }
+        public string dataSource { get; set; }
+        public DateTime createDate { get; set; }
+
+        public List<Stocks> getStocks = new List<Stocks>();
+
+        public void parseOutput()
+        {
+            this.Price = Price;
+            this.Change = Change;
+            
+        }
+    }
+}
